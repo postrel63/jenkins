2c4a2c5b83e5546632e83ee393e8a81e83788890
postrel63 <postrel63@naver.com>
2024-08-23 17:00:09 +0900

    jenkins test 03

--- src/main/java/com/example/jenkins/TestController.java
+++ src/main/java/com/example/jenkins/TestController.java
@@ -11,6 +11,6 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("OK");
+        return ResponseEntity.ok("ver2");
     }
 }
